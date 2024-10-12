8aadc1747a9a3df08db643f37e71c9b2ad9a905a
GLF1211 <109366990+GLF1211@users.noreply.github.com>
2024-10-12 18:17:47 +0800

    new

--- .history/src/settings_20241012181725.h
+++ .history/src/settings_20241012181725.h
@@ -1,13 +1,12 @@
 // 用户信息
-String reqUserKey = "*******";      // 心知天气私钥
-String reqLocation = "******";      // 心知天气城市
-String reqUnit = "c";               // 心知天气摄氏/华氏
+String reqUserKey = "S0zgSchuAgrndPkv4";      // 心知天气私钥
+String reqLocation = "wuhan";      // 心知天气城市
+String reqUnit = "c";
 const char* mqttServer = "test.ranye-iot.net";        // mqtt服务端网址
 static const char ntpServerName[] = "ntp.aliyun.com"; // ntp时间服务器地址
 const int timeZone = 8;                               // 设置时区（8为北京时间）
-String biliUserId = "103589285";           // 哔哩用户id
-String biliBV = "BV1L7411c7jw";            // 哔哩视频BV号
-
+String biliUserId = "350322551";           // 哔哩用户id
+String biliBV = "BV1sQ4y187T3";
 // 运行控制变量
 int bgChgInterval = 60;            // 背景变换时间间隔
 int timeCheckInterval = 300;       // 时间更新时间间隔
